81cb3562d0503bfcfc5fd26747aedb76b086cf79
anamariass <anamariasoaressantana2@gmail.com>
2026-05-28 12:21:46 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -11,7 +11,20 @@ app.get('/sobre', (req, res) => {
   res.send('sobre a aplicação');
 });
 
+app.get('/contato', (req, res) => {
+  res.json({
+  "email": "contato@email.com",
+  "telefone": "(81) 99999-9999"
+});
+});
+
+app.get('/erro', (req, res) => {
+  res.status(404).send('Página não encontrada');
+});
+
+
 app.listen(
     3000, 
     () => console.log(`Servidor em execução`)
 );
+
