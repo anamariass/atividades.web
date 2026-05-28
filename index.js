372e33763efad378090942ec8e40b33477d138bb
anamariass <anamariasoaressantana2@gmail.com>
2026-05-28 12:02:23 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+
 const express = require('express');
 const app = express();
 
+
 app.get('/', (req, res) => {
-  res.send('Bem-vindo ao sistema');
+  res.send('Bem vindo ao sistema');
+});
+
+app.get('/sobre', (req, res) => {
+  res.send('sobre a aplicação');
 });
 
 app.listen(
